1807b18594bc672297368e112e4169431d5fa243
Veena Grace <98600725+veenagrace12@users.noreply.github.com>
2022-08-14 20:53:09 +0530

    Update app.py

--- Flask-App Dev/Note-Taking-App/app.py
+++ Flask-App Dev/Note-Taking-App/app.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,session
+from flask_session import Session
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 notes = []
 @app.route('/', methods=["GET","POST"])
 def index():
+    if session.get("notes") is None:
+        session["notes"] = []
     if request.method == 'POST':
         note = request.form["note"]
-        notes.append(note)
-        return render_template("home.html", notes=notes)
-    return render_template("home.html")
+        if note != "":
+            session["notes"].append(note)
+        
+    return render_template("home.html", notes=session['notes'])
 
 
 if __name__ == '__main__':
