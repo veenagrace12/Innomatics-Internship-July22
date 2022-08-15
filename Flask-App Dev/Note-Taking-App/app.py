2abf4e63d909f3f74727493007a7ccc7b353be33
Veena Grace <98600725+veenagrace12@users.noreply.github.com>
2022-08-15 11:40:36 +0530

    intial commit

--- Flask-App Dev/Note-Taking-App/app.py
+++ Flask-App Dev/Note-Taking-App/app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,session
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 notes = []
 @app.route('/', methods=["GET","POST"])
